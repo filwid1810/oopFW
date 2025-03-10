e25180899e7d978308131f537a38fcb086cfc117
filwid1810 <filipwidenski1810@gmail.com>
2025-03-10 14:24:56 +0100

    lab2

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Point p = new Point();
+        p.x = 2.0;
+        p.y = 3.0;
+        System.out.println(p);
+        //s
+    }
+}
